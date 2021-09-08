10ab9de6a8b30f6a47a27c1d33a05d0361eb9f7c
Ivan Moroziuk <morozyk.ivan@gmail.com>
2021-09-08 19:28:54 +0300

    Implemented bottom navigation

--- app/src/main/java/com/gmail/ivan/morozyk/cookbook/ui/MainActivity.kt
+++ app/src/main/java/com/gmail/ivan/morozyk/cookbook/ui/MainActivity.kt
@@ -3,14 +3,24 @@ package com.gmail.ivan.morozyk.cookbook.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.*
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.gmail.ivan.morozyk.cookbook.navigation.NavigationManager
 import com.gmail.ivan.morozyk.cookbook.navigation.Routes
+import com.gmail.ivan.morozyk.cookbook.navigation.bottomNavItems
 import com.gmail.ivan.morozyk.cookbook.redux.ReduxScreen
 import com.gmail.ivan.morozyk.cookbook.redux.Store
 import com.gmail.ivan.morozyk.cookbook.redux.receiptlist.LoadReceiptList
@@ -29,17 +39,62 @@ class MainActivity : ComponentActivity() {
 
             val navController = rememberNavController()
             navigationManager.attachNavController(navController)
-            Store.dispatch(LoadReceiptList)//not shore if this is the right place to dispatch initial action
+            Store.dispatch(LoadReceiptList)//not sure if this is the right place to dispatch initial action
 
             CookBookTheme {
-                Surface(color = MaterialTheme.colors.background) {
-                    NavHost(
-                        navController = navController,
-                        startDestination = Routes.RECEIPT_LIST.name
-                    ) {
-                        composable(Routes.RECEIPT_LIST.name) {
-                            ReduxScreen(connector = ReceiptListConnector()) { receiptListProps ->
-                                ReceiptListScreen(props = receiptListProps)
+
+                Scaffold(bottomBar = {
+                    BottomNavigation {
+                        val navBackStackEntry by navController.currentBackStackEntryAsState()
+                        val currentDestination = navBackStackEntry?.destination
+
+                        bottomNavItems.forEach { screen ->
+                            BottomNavigationItem(
+                                icon = { Icon(screen.icon, contentDescription = null) },
+                                label = { Text(stringResource(screen.titleRes)) },
+                                selected = currentDestination?.hierarchy?.any { it.route == screen.route.name } == true,
+                                onClick = {
+                                    navController.navigate(screen.route.name) {
+                                        popUpTo(navController.graph.findStartDestination().id) {
+                                            saveState = true
+                                        }
+                                        launchSingleTop = true
+                                        restoreState = true
+                                    }
+                                }
+                            )
+                        }
+                    }
+                }) {
+                    Surface(color = MaterialTheme.colors.background) {
+                        NavHost(
+                            navController = navController,
+                            startDestination = Routes.RECEIPT_LIST.name
+                        ) {
+                            composable(Routes.RECEIPT_LIST.name) {
+                                ReduxScreen(connector = ReceiptListConnector()) { receiptListProps ->
+                                    ReceiptListScreen(props = receiptListProps)
+                                }
+                            }
+
+                            composable(Routes.COLLECTION.name) {
+                                Column(
+                                    modifier = Modifier.fillMaxSize(),
+                                    verticalArrangement = Arrangement.Center,
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
+                                    Text("Collections")
+                                }
+                            }
+
+                            composable(Routes.CART.name) {
+                                Column(
+                                    modifier = Modifier.fillMaxSize(),
+                                    verticalArrangement = Arrangement.Center,
+                                    horizontalAlignment = Alignment.CenterHorizontally
+                                ) {
+                                    Text("Cart")
+                                }
                             }
                         }
                     }
